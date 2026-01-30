d26225d8ce861edd45a5a10c19f71edbca9a3cf2
Bonnie <bonhuynh@umich.edu>
2026-01-29 22:38:34 -0500

    inital app files

---
+++ kiwifruit/kiwifruit/kiwifruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  kiwifruitApp.swift
+//  kiwifruit
+//
+//  Created by Bonnie Huynh on 2026-01-29.
+//
+
+import SwiftUI
+
+@main
+struct kiwifruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
